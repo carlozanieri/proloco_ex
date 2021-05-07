45817741cb9fb8f13562d08ef03aebf6ec4a50fd
carlozanieri <carlo.zanieri@gmail.com>
2021-05-07 10:12:17 +0200

    versione 1.0

--- proloco/controllers/root.py
+++ proloco/controllers/root.py
@@ -122,9 +122,9 @@ class RootController(BaseController):
     def home(self):
         return dict(page='home')
 
-    @expose('proloco.templates.menu_3')
+    @expose('proloco.templates.menu_css')
     def menu(self):
-        return dict(page='menu_3', pagina=Connect.body("", "mugello"))
+        return dict(page='menu_css', pagina=Connect.body("", "mugello"))
 
     @expose('proloco.templates.master')
     def mugello(self):
