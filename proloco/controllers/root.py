be7f08dafe813b1c5ff3846829b6d32e9362cc9c
carlozanieri <carlo.zanieri@gmail.com>
2021-06-07 18:02:14 +0200

    versione 1.0

--- proloco/controllers/root.py
+++ proloco/controllers/root.py
@@ -131,10 +131,10 @@ class RootController(BaseController):
         """Handle the front-page."""
         return dict(page='master', pagina=Connect.body("", "mugello"), luogo="mugello")
 
-    @expose('proloco.templates.master')
+    @expose('proloco.templates.nav')
     def sanpiero(self):
         """Handle the front-page."""
-        return dict(page='master', pagina=Connect.body("", "sanpiero"), luogo="sanpiero")
+        return dict(page='nav', pagina=Connect.body("", "sanpiero"), luogo="sanpiero")
 
     @expose('proloco.templates.master')
     def chisiamo(self):
