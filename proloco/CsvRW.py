51a55af9c2fe6587463bd871097d6e3aeacfb2c8
carlozanieri <carlo.zanieri@gmail.com>
2021-06-16 18:33:15 +0200

    Versione 1.0

---
+++ proloco/CsvRW.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#import os
+#import time
+#import logging
+import os
+import time
+import logging
+import csv
+
+class CsvRW:
+
+   def read(self):
+       import csv
+       with open('./file.csv') as csv_file:
+           csv_reader = csv.reader(csv_file, delimiter=',')
+           line_count = 0
+           for row in csv_reader:
+               if line_count == 0:
+                   print(f'Nomi delle colonne: {", ".join(row)}')
+                   line_count += 1
+               else:
+                   print(f'\t{row[0]} , {row[1]} , {row[2]}.')
+                   line_count += 1
+               print(f'File contiene {line_count} linee.')
+
+   def write(self):
+       import csv
+
+       with open('file2.csv', mode='w') as csv_file:
+           nomicolonne = ['nome', 'cognome', 'citta']
+           writer = csv.DictWriter(csv_file, fieldnames=nomicolonne)
+           writer.writeheader()
+           writer.writerow({'nome': 'Luca', 'cognome': 'Bianchi', 'citta': 'Roma'})
+           writer.writerow({'nome': 'Giovanni', 'cognome': 'Rossi', 'citta': 'Venezia'})
