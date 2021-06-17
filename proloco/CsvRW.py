4904540407b677bf260c77cf8fc35b4593f01f33
carlozanieri <carlo.zanieri@gmail.com>
2021-06-18 01:07:33 +0200

    Versione 1.0

--- proloco/CsvRW.py
+++ proloco/CsvRW.py
@@ -19,7 +19,7 @@ class CsvRW:
                     nomicolonne = ['COGNOME',  'E-MAIL']
                     writer = csv.DictWriter(csv_file, fieldnames=nomicolonne)
                     writer.writeheader()
-                    data= [ row[2] + " " + row[3], row[4]]
+                    data= [ {'COGNOME': row[2] + " " + row[3] , 'NOME': row[3], 'E-MAIL': row[4]}]
                     writer.writerow(data)
                     ##writer.writerow({'COGNOME': row[2] + " " + row[3] , 'E-MAIL': row[4]})
                     print({'COGNOME': row[2] + " " + row[3] , 'NOME': row[3], 'E-MAIL': row[4]})
