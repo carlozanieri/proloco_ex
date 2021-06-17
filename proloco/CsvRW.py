6bf74e63d35df586518e093f0c8e88ac7afa19d4
carlozanieri <carlo.zanieri@gmail.com>
2021-06-17 18:43:03 +0200

    Versione 1.0

--- proloco/CsvRW.py
+++ proloco/CsvRW.py
@@ -10,7 +10,6 @@ import csv
 class CsvRW:
 
    def read(self):
-       import csv
        with open('file.csv') as csv_file:
            csv_reader = csv.reader(csv_file, delimiter=',')
            line_count = 0
@@ -19,21 +18,10 @@ class CsvRW:
                    print(f'Nomi delle colonne: {", ".join(row)}')
                    line_count += 1
                else:
-                   with open('file5.csv', mode='w') as csv_file:
-                       nomicolonne = ['nome', 'cognome', 'citta']
-                       writer = csv.DictWriter(csv_file, fieldnames=nomicolonne)
-                       writer.writeheader()
-                       writer.writerow({'nome': 'Luca', 'cognome': 'Bianchi', 'citta': 'Roma'})
-                       writer.writerow({'nome': 'Giovanni', 'cognome': 'Rossi', 'citta': 'Venezia'})
-
+                   print(f'\t{row[0]} , {row[1]} , {row[2]}.')
                    line_count += 1
                print(f'File contiene {line_count} linee.')
-               with open('file5.csv', mode='w') as csv_file:
-                   nomicolonne = ['nome', 'cognome', 'citta']
-                   writer = csv.DictWriter(csv_file, fieldnames=nomicolonne)
-                   writer.writeheader()
-                   writer.writerow({'nome': 'Luca', 'cognome': 'Bianchi', 'citta': 'Roma'})
-                   writer.writerow({row[0]} , {row[1]} , {row[2]})
+
    def write(self):
        import csv
 
